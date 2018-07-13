6b1c7534d51b284a3d1c462a8e9358550ff4cf4c
Geewon Lee <gwlee1991@gmail.com>
2018-07-12 22:34:45 -0700

    requiring lodash

--- require/app.js
+++ require/app.js
@@ -1,15 +1,19 @@
 console.log('running app.js');
 const fs = require('fs');
-const os = require('os');
+// const os = require('os');
 const notes = require('./note');
+const _ = require('lodash');
 
-var user = os.userInfo();
+// var user = os.userInfo();
 // console.log(user);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
-  if (err) {
-    console.log(err);
-  }
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
 
-var result = notes.addNote();
+// var result = notes.addNote();
+
+// console.log(_.isString('hi'));
+// console.log(_.isString(3));
